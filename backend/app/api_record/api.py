9c79df6471782a741e3a85965f3898c115686a40
Wang Shenghe <h469hr@inf.elte.hu>
2023-04-30 13:13:56 +0200

    recommended questions

--- backend/app/api_record/api.py
+++ backend/app/api_record/api.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
+import random
 
 from app import db
 from flask import request, jsonify, current_app, session
@@ -117,7 +118,39 @@ class RecordDeleteAPI(MethodView):
             return jsonify(ret_json)
 
 
+class RecordRecommendAPI(MethodView):
+    """
+    Get some recommended records API
+    """
+
+    def get(self):
+        """
+        Get some recommended records.
+        swagger_from_file: app/api_record/docs/record_recommend.yml
+        """
+        ret_json = {
+            "status": 200,
+            "message": 'Get the questions successfully',
+            "request": request.base_url,
+            "data": {}
+        }
+
+        recommended_records = Record.query.all()
+        len_records = len(recommended_records)
+        recommended_index = []
+        for i in range(7):
+            r = random.randint(0, len_records - 1)
+            if r not in recommended_index:
+                recommended_index.append(r)
+        recommended_list = [recommended_records[i].content.split('\n')[0] for i in recommended_index]
+        ret_json.update({'data': {'recommend_questions': recommended_list}})
+        print(ret_json)
+
+        return jsonify(ret_json)
+
+
 api_record.add_url_rule('/add', view_func=RecordStoreAPI.as_view('add'))
 api_record.add_url_rule('/record_list', view_func=RecordListAPI.as_view('record_list'))
 api_record.add_url_rule('/delete_record', view_func=RecordDeleteAPI.as_view('delete_record'))
+api_record.add_url_rule('/recommended_question', view_func=RecordRecommendAPI.as_view('recommended_question'))
 
